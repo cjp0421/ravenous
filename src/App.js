c7b97f7eccf73dc40efbfe5041608e59c20601e1
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-05 10:11:19 -0500

    added Business component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Business from './Business.js';
+
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Business />
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Business.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Business() {
+    return <h1>I'm a business!</h1>
+}
+
+export default Business;
