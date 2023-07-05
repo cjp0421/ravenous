5497fabbde8619d1b85905774d6a82fa7cef35d0
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-05 11:04:43 -0500

    Updated Business component
    practicing with component to review react

--- src/Business.js
+++ src/Business.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+const business = {
+    image: 'image',
+    name: 'business',
+    address: 'address',
+    city: 'city',
+    state: 'state',
+    zipcode: 'zip',
+    category: 'category',
+    rating: 'rating',
+    reviewCount: 0
+};
+
 function Business() {
-    return <h1>I'm a business!</h1>
+
+
+    return (
+        <>
+            <h1>I'm a business component!</h1>
+            <p>{business.name}</p>
+            <p>{business.reviewCount}</p>
+        </>)
 }
 
 export default Business;
