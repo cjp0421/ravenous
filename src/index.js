fe8d99699fc80d1fcdbc36af8cc600b60e83528d
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-06 14:27:07 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <App />
