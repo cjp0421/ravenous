fef59a77c25677d0d31eb316e42a4543e7541942
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-06 10:55:02 -0500

    updated Hero, Navbar components, added Google Font
    also added new banner image

--- src/Hero.js
+++ src/Hero.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './Hero.css';
-import logo from './images/chillipepper.jpg';
+
 
 export default function Hero() {
     return (
         <>
             <div class="text-center" class="heroImage">
-                <img src={logo} class="image" alt="background image" />
+
                 <h1>ravenous</h1>
             </div>
         </>
