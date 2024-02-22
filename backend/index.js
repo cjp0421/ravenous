993495d5c747ddc46b2aff9556e1d7ab03dd333b
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-02-22 13:15:51 -0600

    trying a new approach

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 require('dotenv').config();
 const apiKey = process.env.YELP_API_KEY;
 
-
+if (!apiKey) {
+    console.error('Yelp API key is missing. Set the REACT_APP_YELP_API_KEY environment variable.');
+    process.exit(1); // Exit the process with an error code
+}
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,13 +19,36 @@ app.use(cors());
 // Use bodyParser if needed
 app.use(express.json());
 
+const yelpApiUrl = 'https://api.yelp.com/v3/businesses/search';
+
+app.get('/search', async (req, res) => {
+    try {
+        const searchTerm = req.query.term || 'food'; // Default term is 'food', you can change it based on your requirements
+        const location = req.query.location || 'Los Angeles'; // Default location is 'Los Angeles', you can change it based on your requirements
+
+        const response = await axios.get(yelpApiUrl, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+            params: {
+                location: location || "saint charles",
+                term: searchTerm || "soup",
+
+            },
+        });
+
+        const businesses = response.data.businesses;
+        res.json(businesses);
+    } catch (error) {
+        console.error('Error fetching data from Yelp API', error.response ? error.response.data : error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-if (!apiKey) {
-    console.error('Yelp API key is missing. Set the REACT_APP_YELP_API_KEY environment variable.');
-    process.exit(1); // Exit the process with an error code
-}
 
